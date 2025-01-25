38918fe7d0cc8490eba4021bc6a26386906c67ba
Edwin Allauca <edwindavid21v@gmail.com>
2025-01-25 15:37:03 -0500

    fix: :bug: Add files swagger and fix app.js file

--- inventoryObjectsCreate/src/app.js
+++ inventoryObjectsCreate/src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const inventoryRoutes = require('./routes/inventoryRoutes');
 
 dotenv.config();
@@ -10,12 +12,17 @@ const port = process.env.PORT || 3014;
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Allows handling of JSON in requests
+app.use(express.json());
+
+// Swagger setup
+const swaggerDocument = YAML.load('./src/docs/swagger.yaml');  // Ruta del archivo swagger.yaml
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Routes
 app.use('/api', inventoryRoutes);
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
+  console.log(`Swagger docs available at http://localhost:${process.env.PORT}/api-docs`);
 });
