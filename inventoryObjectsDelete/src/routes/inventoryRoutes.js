6f74cc32119c688b6c1705764b01637571984d66
Edwin Allauca <edwindavid21v@gmail.com>
2025-02-09 16:59:38 -0500

    feat: :sparkles: Add new files and configurations
    Add new files an d configurations

--- inventoryObjectsDelete/src/routes/inventoryRoutes.js
+++ inventoryObjectsDelete/src/routes/inventoryRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const inventoryController = require('../controllers/inventoryController');
+const { authorize } = require('../Middlewares/authMiddleware');
 const router = express.Router();
 
 // DELETE route to delete an object
-router.delete('/:id', inventoryController.deleteInventoryObject);
+router.delete('/:id', inventoryController.deleteInventoryObject, authorize);
 
 module.exports = router;
