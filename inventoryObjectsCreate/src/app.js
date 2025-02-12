b0cd86bbc8ef0bc5e1fc1ccdd67c8d706c0ec841
Edwin Allauca <edwindavid21v@gmail.com>
2025-02-12 18:50:14 -0500

    feat: :sparkles: Add configurations for prometheus and grafana.
    Add configurations for prometheus and grafana.

--- inventoryObjectsCreate/src/app.js
+++ inventoryObjectsCreate/src/app.js
@@ -4,15 +4,64 @@ const dotenv = require('dotenv');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
 const inventoryRoutes = require('./routes/inventoryRoutes');
+const client = require('prom-client');
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORTC || 3014;
 
+const collectDefaultMetrics = client.collectDefaultMetrics;
+
 // Middlewares
 app.use(cors());
 app.use(express.json());
+
+// Configuring Prometheus metrics.
+collectDefaultMetrics();
+
+// Request counter
+const httpRequestCounter = new client.Counter({
+  name: "http_requests_total",
+  help: "Total HTTP requests received",
+  labelNames: ["method", "route", "status_code"],
+});
+
+// Histogram for response times
+const httpRequestDuration = new client.Histogram({
+  name: "http_request_duration_seconds",
+  help: "Histogram for the duration of HTTP requests in seconds",
+  labelNames: ["method", "route", "status_code"],
+  buckets: [0.1, 0.5, 1, 2, 5],
+});
+
+//MMiddleware to measure metrics on each request
+app.use((req, res, next) => {
+  const start = Date.now();
+
+  res.on("finish", () => {
+    const duration = (Date.now() - start) / 1000;
+
+    httpRequestCounter.inc({
+      method: req.method,
+      route: req.path,
+      status_code: res.statusCode,
+    });
+
+    httpRequestDuration.observe(
+      { method: req.method, route: req.path, status_code: res.statusCode },
+      duration
+    );
+  });
+
+  next();
+});
+
+// Metrics endpoint.
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
 
 // Swagger setup
 const swaggerDocument = YAML.load('./src/docs/swagger.yaml');  // Route swagger.yaml
